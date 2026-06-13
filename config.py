a3d613a972cdb8607857fc64961e78c3f64b2e55
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-06-14 00:13:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-#ALONE CODER
 from os import getenv
 from dotenv import load_dotenv
 
@@ -6,43 +5,79 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", "17596251"))
-        self.API_HASH = getenv("API_HASH", "e58343b4c0193e293e391daf97603fcd")
+        self.API_ID = int(getenv("API_ID", 0))
+        self.API_HASH = getenv("API_HASH")
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN", "Apna Bot Token")
-        self.MONGO_URL = getenv("MONGO_URL", "Apna Mongo Db Dalo")
+        self.BOT_TOKEN = getenv("BOT_TOKEN")
+        self.MONGO_URL = getenv("MONGO_URL")
 
-        self.LOGGER_ID = int(getenv("LOGGER_ID", "Apna Log Group Id Dalo"))
-        self.OWNER_ID = int(getenv("OWNER_ID", "Owner I'd dalo"))
-        
-        self.SESSION1 = getenv("SESSION", "Apna String Dalo")
+        self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
+        self.OWNER_ID = int(getenv("OWNER_ID", 0))
+
+        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 17000)) * 60
+        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 200))
+        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 200))
+
+        self.SESSION1 = getenv("SESSION", None)
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
         self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/shona_bots")
         self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/shona_support")
 
-        self.AUTO_END: bool = getenv("AUTO_END", False)
-        self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
-        self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", True)
+        self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
+        self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
 
-        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "200"))
-        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", "17000"))
-        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", "200"))
-        self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INFLEX68575028D")
+        self.THUMB_GEN: bool = getenv("THUMB_GEN", "True").lower() == "true"
+        self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", "True").lower() == "true"
+
+        self.LANG_CODE = getenv("LANG_CODE", "en")
+
         self.COOKIES_URL = [
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
         ]
-        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://files.catbox.moe/s5orbf.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/s5orbf.jpg")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/s5orbf.jpg")
+
+        self.DEFAULT_THUMB = getenv(
+            "DEFAULT_THUMB",
+            "https://te.legra.ph/file/3e40a408286d4eda24191.jpg"
+        )
+        self.PING_IMG = getenv(
+            "PING_IMG",
+            "https://files.catbox.moe/haagg2.png"
+        )
+        self.START_IMG = getenv(
+            "START_IMG",
+            "https://files.catbox.moe/zvziwk.jpg"
+        )
+
+        # API End Point
+        self.YTPROXY_URL = getenv(
+            "YTPROXY_URL",
+            "https://tgapi.xbitcode.com"
+        )
+
+        self.YT_API_KEY = getenv(
+            "YT_API_KEY",
+            "xbit_acgCNcQCN1wYWWlpteKhTpwiVTFJYhFl"
+        )
 
     def check(self):
         missing = [
             var
-            for var in ["API_ID", "API_HASH", "BOT_TOKEN", "MONGO_URL", "LOGGER_ID", "OWNER_ID", "SESSION1"]
+            for var in [
+                "API_ID",
+                "API_HASH",
+                "BOT_TOKEN",
+                "MONGO_URL",
+                "LOGGER_ID",
+                "OWNER_ID",
+                "SESSION1"
+            ]
             if not getattr(self, var)
         ]
+
         if missing:
-            raise SystemExit(f"Missing required environment variables: {', '.join(missing)}")
+            raise SystemExit(
+                f"Missing required environment variables: {', '.join(missing)}"
+        )
