3a5d96b8d842bdb3ace87edf0c52fcad04843e1d
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-07-10 08:28:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/shona_bots")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/shona_support")
         self.OWNER_USERNAME = getenv("OWNER_USERNAME", "https://t.me/thecderqueen")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
@@ -30,7 +30,7 @@ class Config:
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "200"))
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", "17000"))
         self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", "200"))
-        self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INFLEX68575028D")
+        self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INFLEX86759628D")
         self.COOKIES_URL = [
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
